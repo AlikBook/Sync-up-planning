b68d9087b4b2a81f8e20ccb3dd1ed2a4491a8242
Ivan <ivanarturograndi6@gmail.com>
2026-04-21 14:55:05 +0200

    chore : updated name of the website

--- lib/main.dart
+++ lib/main.dart
@@ -17,7 +17,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Planning',
+      title: 'SyncUp',
       theme: ThemeData(
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.lightBlue),
         primarySwatch: Colors.blue,
